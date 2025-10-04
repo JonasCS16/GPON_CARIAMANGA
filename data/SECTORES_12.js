5ff6e70fd165be93f4c5ad308a72bc0c1aae8f96
JonasCS16 <jonascs1692@gmail.com>
2025-10-04 11:34:13 -0500

    Add files via upload

---
+++ data/SECTORES_12.js
@@ -0,0 +1,23 @@
+var json_SECTORES_12 = {
+"type": "FeatureCollection",
+"name": "SECTORES_12",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "El Dulce" }, "geometry": { "type": "Point", "coordinates": [ -79.967660482339369, -4.040072727301742 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Sabanilla" }, "geometry": { "type": "Point", "coordinates": [ -80.129725259262457, -4.202202078825969 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Quillusara" }, "geometry": { "type": "Point", "coordinates": [ -80.125387178845912, -4.170566986508716 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "La Rota" }, "geometry": { "type": "Point", "coordinates": [ -80.116896746226573, -4.133568455051552 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Saucillo" }, "geometry": { "type": "Point", "coordinates": [ -80.196576766824407, -4.274739242982358 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Potrerillos" }, "geometry": { "type": "Point", "coordinates": [ -80.123686034374671, -4.268450597223687 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Algodonal" }, "geometry": { "type": "Point", "coordinates": [ -80.051090393542182, -4.281375857571919 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Vicin" }, "geometry": { "type": "Point", "coordinates": [ -80.109891412193804, -4.26887607626896 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Jaguay" }, "geometry": { "type": "Point", "coordinates": [ -80.152497008164929, -4.264514993693906 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Hacienda Vieja" }, "geometry": { "type": "Point", "coordinates": [ -80.204937577300626, -4.26774332992921 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Pasaje de Jujal" }, "geometry": { "type": "Point", "coordinates": [ -80.04083079385164, -4.2899714738018 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "BADEAL" }, "geometry": { "type": "Point", "coordinates": [ -79.960813663986485, -4.379717512470592 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "LA CRUZ" }, "geometry": { "type": "Point", "coordinates": [ -79.97776765750308, -4.368507774717497 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "MACHANGUILLA" }, "geometry": { "type": "Point", "coordinates": [ -79.988525975751017, -4.345899597287089 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "LA GUATARA" }, "geometry": { "type": "Point", "coordinates": [ -79.974632892002916, -4.326269290619608 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Cascajal" }, "geometry": { "type": "Point", "coordinates": [ -79.591154785341331, -4.321455737280703 ] } }
+]
+}
